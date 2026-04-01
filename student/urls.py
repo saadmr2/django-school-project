e11dd1fa7c092090912bf0b8f98bd2a39d0171d7
Saad Marzouq <saadmarzouq06@gmail.com>
2026-04-01 20:34:26 +0100

    Complete student CRUD with templates

--- student/urls.py
+++ student/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('students/', views.student_list, name='student_list'),
     path('add-student/', views.add_student, name='add_student'),
+    path('edit-student/<int:id>/', views.edit_student, name='edit_student'),
+    path('delete-student/<int:id>/', views.delete_student, name='delete_student'),
 ]
--- student/views.py
+++ student/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from .models import Student, Parent
 
+
 def student_list(request):
     students = Student.objects.all()
     return render(request, 'students/students.html', {'students': students})
+
 
 def add_student(request):
     parents = Parent.objects.all()
@@ -15,7 +17,18 @@ def add_student(request):
         adresse = request.POST.get('adresse')
         parent_id = request.POST.get('parent')
 
-        parent = Parent.objects.get(id=parent_id)
+        if not parent_id:
+            return render(request, 'students/add-student.html', {
+                'parents': parents,
+                'error': 'Please select a parent.'
+            })
+
+        parent = Parent.objects.filter(id=parent_id).first()
+        if not parent:
+            return render(request, 'students/add-student.html', {
+                'parents': parents,
+                'error': 'Selected parent does not exist.'
+            })
 
         Student.objects.create(
             nom=nom,
@@ -28,3 +41,45 @@ def add_student(request):
         return redirect('student_list')
 
     return render(request, 'students/add-student.html', {'parents': parents})
+
+
+def edit_student(request, id):
+    student = Student.objects.get(id=id)
+    parents = Parent.objects.all()
+
+    if request.method == 'POST':
+        student.nom = request.POST.get('nom')
+        student.prenom = request.POST.get('prenom')
+        student.age = request.POST.get('age')
+        student.adresse = request.POST.get('adresse')
+
+        parent_id = request.POST.get('parent')
+        if not parent_id:
+            return render(request, 'students/edit-student.html', {
+                'student': student,
+                'parents': parents,
+                'error': 'Please select a parent.'
+            })
+
+        parent = Parent.objects.filter(id=parent_id).first()
+        if not parent:
+            return render(request, 'students/edit-student.html', {
+                'student': student,
+                'parents': parents,
+                'error': 'Selected parent does not exist.'
+            })
+
+        student.parent = parent
+        student.save()
+        return redirect('student_list')
+
+    return render(request, 'students/edit-student.html', {
+        'student': student,
+        'parents': parents
+    })
+
+
+def delete_student(request, id):
+    student = Student.objects.get(id=id)
+    student.delete()
+    return redirect('student_list')
