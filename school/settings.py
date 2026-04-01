ee9e943996a23a7eba6c2e9bb771a11d25a2f372
yassineSohayly <yassinsohayly1@gmail.com>
2026-04-01 01:11:26 +0100

    Configure templates and static files

--- school/settings.py
+++ school/settings.py
@@ -115,4 +115,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+# STATIC_URL = 'static/'
+# import os
+
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
