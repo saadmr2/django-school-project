15e91916a3cebd51fc34d36b7544510a5dad22c1
Saad Marzouq <saadmarzouq06@gmail.com>
2026-04-01 14:08:55 +0100

    Fix add student form and connect to model

--- student/urls.py
+++ student/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('students/', views.student_list, name='student_list'),
+    path('add-student/', views.add_student, name='add_student'),
 ]
--- student/views.py
+++ student/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
-from .models import Student
+from django.shortcuts import render, redirect
+from .models import Student, Parent
 
 def student_list(request):
     students = Student.objects.all()
     return render(request, 'students/students.html', {'students': students})
+
+def add_student(request):
+    parents = Parent.objects.all()
+
+    if request.method == 'POST':
+        nom = request.POST.get('nom')
+        prenom = request.POST.get('prenom')
+        age = request.POST.get('age')
+        adresse = request.POST.get('adresse')
+        parent_id = request.POST.get('parent')
+
+        parent = Parent.objects.get(id=parent_id)
+
+        Student.objects.create(
+            nom=nom,
+            prenom=prenom,
+            age=age,
+            adresse=adresse,
+            parent=parent
+        )
+
+        return redirect('student_list')
+
+    return render(request, 'students/add-student.html', {'parents': parents})
