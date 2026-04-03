215e4d5b21abbd0a55194caba6af34f06de396bd
saadmarzouq <161056379+saadmr2@users.noreply.github.com>
2026-04-03 19:53:35 +0100

    Add files via upload

--- faculty/admin.py
+++ faculty/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Teacher, Department, Holiday, Subject, Timetable
 
-# Register your models here.
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('teacher_id', 'first_name', 'last_name', 'gender', 'mobile_number')
+    search_fields = ('teacher_id', 'first_name', 'last_name')
+    list_filter = ('gender',)
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'head_teacher')
+    search_fields = ('name',)
+    filter_horizontal = ('teachers',)
+
+@admin.register(Holiday)
+class HolidayAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date', 'description')
+    search_fields = ('name',)
+    list_filter = ('date',)
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code', 'department', 'teacher')
+    search_fields = ('name', 'code')
+
+@admin.register(Timetable)
+class TimetableAdmin(admin.ModelAdmin):
+    list_display = ('day', 'start_time', 'end_time', 'subject', 'teacher', 'room')
+    list_filter = ('day', 'teacher')
+    ordering = ('day', 'start_time')
